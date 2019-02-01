f559c188f9b60cc2517a9a1043fc64f1b030499e
adithak <adithak59@gmail.com>
2019-02-01 11:36:17 +0530

    Create aa.c

---
+++ aa.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main(int arg, char *argv[])
+{
+ size_t maxlen = 0, leng;
+ int j;
+ int longest;
+ for (j = 1; j < arg; j++) 
+  {
+  len = strlen(argv[j]);
+  if (leng>maxlen) 
+  longest = argv[j];
+ }
+printf("longest string is %s. \n", longest);
+return 0;
+}
